a3174b288f0fa57a92cc54c9f8b43fc25dbaca70
matthunz <matthunz2@gmail.com>
2016-08-13 01:36:03 -0400

    Add toml parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 extern crate rustache;
+extern crate toml;
 
 use rustache::{HashBuilder, render_file};
 use std::io::Read;
-use std::process;
+use std::process::exit;
 
 fn main() {
+    let toml = r#"
+        [test]
+        foo = "bar"
+    "#;
+
+    let config = match toml::Parser::new(toml).parse() {
+        Some(config) => config,
+        None => {
+            println!("error: could not parse configuration file");
+            exit(1);
+        }
+    };
+
+    println!("{:?}", config.get("test").unwrap());
+    
     let data = HashBuilder::new()
         .insert_string("fg", "#888888");
     
@@ -15,7 +31,7 @@ fn main() {
         },
         Err(err) => {
             println!("error: could not read template file");
-            process::exit(1);
+            exit(1);
         }
     };
     
