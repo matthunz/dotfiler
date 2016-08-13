d21bf416874748b80c33e28a5b84372e0f584f26
matthunz <matthunz2@gmail.com>
2016-08-13 00:58:36 -0400

    Add template file rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate rustache;
+
+use rustache::{HashBuilder, render_file};
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let data = HashBuilder::new()
+        .insert_string("fg", "#888888");
+    
+    let mut rendered = String::new();
+    match rustache::render_file("examples/xresources", data) {
+        Ok(mut render) => {
+            render.read_to_string(&mut rendered);
+        },
+        Err(err) => {
+            println!("Error: could not read template file");
+        }
+    };
+    
+    println!("{}", rendered);
 }
